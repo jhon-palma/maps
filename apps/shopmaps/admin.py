34e5c7e36e3f0e2d6538f707e4a6c560d0ee8a91
Jhon Palma <jhon-palma@upc.edu.co>
2025-11-08 14:06:10 -0500

    Modified admin

--- apps/shopmaps/admin.py
+++ apps/shopmaps/admin.py
@@ -1,5 +1,6 @@
-from apps.shopmaps.models.stores import State, City
+from apps.shopmaps.models.stores import State, City, Stores
 from django.contrib import admin
 
 admin.site.register(State)
 admin.site.register(City)
+admin.site.register(Stores)
